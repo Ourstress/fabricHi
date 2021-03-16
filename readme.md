b35b6d41f56949a64fae66fbf19e7593286df140
Ourstress <sgng1@e.ntu.edu.sg>
2021-03-16 04:21:12 +0000

    got docker running in separate shell

--- readme.md
+++ readme.md
@@ -2,3 +2,6 @@
 
 * start of the day run `chmod u+x start.sh`
 * then we can execute the shell script using `./start.sh`
+
+After running network.sh, we should see
+![image](https://user-images.githubusercontent.com/3660908/111255411-f7a21680-8651-11eb-9bc1-f8bb5ce4a740.png)
--- start.sh
+++ start.sh
@@ -2,14 +2,10 @@
 
 start()
 {
-echo "Starting program at $(date)" # Date will be substituted
-
-sudo docker-up # start docker
-
 curl -sSL https://bit.ly/2ysbOFE | bash -s # download fabric
 
 cd fabric-samples/test-network 
 ./network.sh down
 ./network.sh up createChannel # get channel started
 }
-start
+sudo docker-up & start # docker will be run in a separate shell from start shell function
